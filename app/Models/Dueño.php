5e89a72c398ba31f8f89037fb4e5b18c821a6ab5
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-04-20 14:59:50 -0500

    Creación de Modelos

---
+++ app/Models/Dueño.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dueño extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'apellido', 'direccion', 'telefono', 'email'];
+
+    public function mascotas()
+    {
+        return $this->hasMany(Mascota::class);
+    }
+}
+
---
+++ app/Models/Mascota.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mascota extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['nombre', 'especie', 'raza', 'edad', 'dueño_id'];
+
+    public function dueño()
+    {
+        return $this->belongsTo(Dueño::class, 'dueño_id');
+    }
+
+    public function visitas()
+    {
+        return $this->hasMany(Visita::class);
+    }
+}
