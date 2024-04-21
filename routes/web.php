14be46f0ce76095f0471770a3d36d90421cb133f
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-04-20 21:57:01 -0500

    Creando Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\DueñoController;
+use App\Http\Controllers\MascotaController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VisitaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +19,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('dueños', DueñoController::class);
+Route::resource('mascotas', MascotaController::class);
+Route::resource('visitas', VisitaController::class);
 
 require __DIR__.'/auth.php';
