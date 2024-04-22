6ec45f6d380636248bf9dcd37afdf31c4873389a
Alexandro Yule Valencia <alexandroyule@gmail.com>
2024-04-21 20:13:54 -0500

    Proteger Rutas

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('mascotas', MascotaController::class);
+    Route::resource('dueños', DueñoController::class);
+    Route::resource('visitas', VisitaController::class);
 });
 
-Route::resource('dueños', DueñoController::class);
-Route::resource('visitas', VisitaController::class);
 
 require __DIR__.'/auth.php';
