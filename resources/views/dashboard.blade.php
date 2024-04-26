de8c6088bfbcdebc238cbbf47a1e5de0ad30db10
Luis Donado <luisinodnd@gmail.com>
2024-04-26 17:24:31 -0500

    Imagenes del dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
     <div class="col-md-3">
         <a href="{{ route('mascotas.index') }}" class="text-decoration-none">
             <div class="card">
+                <img src="{{asset('assets/images/mascotas.jpeg')}}" class="card-img-top" alt="Imagen de Mascotas">
                 <div class="card-body">
                     <h5 class="card-title">Mascotas</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas iste enim explicabo at illo vel? Sit, voluptas consectetur tenetur vero, ducimus consequuntur porro culpa debitis assumenda sunt voluptatibus nemo vel.</p>
@@ -17,6 +18,7 @@
     <div class="col-md-3">
         <a href="{{ route('dueños.index') }}" class="text-decoration-none">
             <div class="card">
+                <img src="{{asset('assets/images/dueños.jpeg')}}" class="card-img-top" alt="Imagen de Dueños">
                 <div class="card-body">
                     <h5 class="card-title">Dueños</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti neque quia ab fugiat voluptates nisi voluptatem fugit dolore sunt? Commodi amet aut iure blanditiis quibusdam delectus ipsum voluptas obcaecati nulla.</p>
@@ -27,6 +29,7 @@
     <div class="col-md-3">
         <a href="{{ route('visitas.index') }}" class="text-decoration-none">
             <div class="card">
+                <img src="{{asset('assets/images/visitas.jpeg')}}" class="card-img-top" alt="Imagen de Visitas">
                 <div class="card-body">
                     <h5 class="card-title">Visitas</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. In modi voluptatem dolore cum optio? Magni esse optio hic eaque quae repellendus accusantium incidunt, dicta eum ullam quisquam similique. Delectus, repellat?</p>
